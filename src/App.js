8e948174e7764edcaf187464d911ff40656d5258
arseniy_pomazkov <arpomazkov@bk.ru>
2021-02-01 18:54:30 +0300

    Set up a react project of an online coffee shop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <div className="container">
+      <header>
+          <div className="header-logo">
+              <img src="images/coffe-guys-logo.png" alt="" className="header-logo-img" />
+              <div className="header-logo-text">
+                  <h2>Coffee guys</h2>
+                  <p>the freshest coffee from enthusiasts</p>
+              </div>
+          </div>
+          <button className="header-cart-button">
+              <p className="header-cart-button-price">520 ₽</p>
+              <img className="header-cart-button-cart_icon" src="images/shopping-cart.svg" alt="" />
+              <p>3</p>
+          </button>
       </header>
-    </div>
+      <div className="content">
+          <nav className="filters">
+              <div className="filters-div">
+                  <button className="filters-button filters-button-active">All</button>
+                  <button className="filters-button">Espresso</button>
+                  <button className="filters-button">Filter</button>
+                  <button className="filters-button">Capsules</button>
+                  <button className="filters-button">Drip bags</button>
+              </div>
+              <div className="filters-sorting">
+                  <img className="filters-sorting-triangle" src="images/triangle.svg" alt="" />
+                  <p className="filters-sorting-label">Sorted by: </p>
+                  <p className="filters-sorting-method">popularity</p>
+                  <div className="filters-sorting-menu">
+                      <div className="filters-sorting-menu-item filters-sorting-menu-item-active"><p>popularity</p></div>
+                      <div className="filters-sorting-menu-item"><p>price</p></div>
+                      <div className="filters-sorting-menu-item"><p>q-grade</p></div>
+                  </div>
+              </div>
+          </nav>
+          <div className="products">
+              <div className="product-card">
+                  <img src="images/uganda-sipi-falls-organic-025.png" alt="" className="product-card-img" />
+                  <h3 className="product-card-name">Uganda Sipi Falls</h3>
+                  <div className="product-card-options">
+                      <ul className="product-card-options-grind">
+                          <li className="active">in grains</li>
+                          <li className="">pre-ground</li>
+                      </ul>
+                      <ul className="product-card-options-size">
+                          <li className="active">200g</li>
+                          <li className="">500g</li>
+                          <li className="">1kg</li>
+                      </ul>
+                  </div>
+                  <div className="product-card-bottom">
+                      <h3 className="product-card-price">545 rub</h3>
+                      <button className="product-card-button"><strong>+</strong> Add <div className="added_amount active">3</div></button>
+                  </div>
+              </div>
+              <div className="product-card">
+                  <img src="images/uganda-sipi-falls-organic-025.png" alt="" className="product-card-img" />
+                  <h3 className="product-card-name">Uganda Sipi Falls</h3>
+                  <div className="product-card-options">
+                      <ul className="product-card-options-grind">
+                          <li className="active">in grains</li>
+                          <li className="">pre-ground</li>
+                      </ul>
+                      <ul className="product-card-options-size">
+                          <li className="active">200g</li>
+                          <li className="">500g</li>
+                          <li className="">1kg</li>
+                      </ul>
+                  </div>
+                  <div className="product-card-bottom">
+                      <h3 className="product-card-price">545 rub</h3>
+                      <button className="product-card-button"><strong>+</strong> Add <div className="added_amount active">3</div></button>
+                  </div>
+              </div>
+              <div className="product-card">
+                  <img src="images/uganda-sipi-falls-organic-025.png" alt="" className="product-card-img" />
+                  <h3 className="product-card-name">Uganda Sipi Falls</h3>
+                  <div className="product-card-options">
+                      <ul className="product-card-options-grind">
+                          <li className="active">in grains</li>
+                          <li className="">pre-ground</li>
+                      </ul>
+                      <ul className="product-card-options-size">
+                          <li className="active">200g</li>
+                          <li className="">500g</li>
+                          <li className="">1kg</li>
+                      </ul>
+                  </div>
+                  <div className="product-card-bottom">
+                      <h3 className="product-card-price">545 rub</h3>
+                      <button className="product-card-button"><strong>+</strong> Add <div className="added_amount active">3</div></button>
+                  </div>
+              </div>
+              <div className="product-card">
+                  <img src="images/uganda-sipi-falls-organic-025.png" alt="" className="product-card-img" />
+                  <h3 className="product-card-name">Uganda Sipi Falls</h3>
+                  <div className="product-card-options">
+                      <ul className="product-card-options-grind">
+                          <li className="active">in grains</li>
+                          <li className="">pre-ground</li>
+                      </ul>
+                      <ul className="product-card-options-size">
+                          <li className="active">200g</li>
+                          <li className="">500g</li>
+                          <li className="">1kg</li>
+                      </ul>
+                  </div>
+                  <div className="product-card-bottom">
+                      <h3 className="product-card-price">545 rub</h3>
+                      <button className="product-card-button"><strong>+</strong> Add <div className="added_amount active">3</div></button>
+                  </div>
+              </div>
+              <div className="product-card">
+                  <img src="images/uganda-sipi-falls-organic-025.png" alt="" className="product-card-img" />
+                  <h3 className="product-card-name">Uganda Sipi Falls</h3>
+                  <div className="product-card-options">
+                      <ul className="product-card-options-grind">
+                          <li className="active">in grains</li>
+                          <li className="">pre-ground</li>
+                      </ul>
+                      <ul className="product-card-options-size">
+                          <li className="active">200g</li>
+                          <li className="">500g</li>
+                          <li className="">1kg</li>
+                      </ul>
+                  </div>
+                  <div className="product-card-bottom">
+                      <h3 className="product-card-price">545 rub</h3>
+                      <button className="product-card-button"><strong>+</strong> Add <div className="added_amount active">3</div></button>
+                  </div>
+              </div>
+              <div className="product-card">
+                  <img src="images/uganda-sipi-falls-organic-025.png" alt="" className="product-card-img" />
+                  <h3 className="product-card-name">Uganda Sipi Falls</h3>
+                  <div className="product-card-options">
+                      <ul className="product-card-options-grind">
+                          <li className="active">in grains</li>
+                          <li className="">pre-ground</li>
+                      </ul>
+                      <ul className="product-card-options-size">
+                          <li className="active">200g</li>
+                          <li className="">500g</li>
+                          <li className="">1kg</li>
+                      </ul>
+                  </div>
+                  <div className="product-card-bottom">
+                      <h3 className="product-card-price">545 rub</h3>
+                      <button className="product-card-button"><strong>+</strong> Add <div className="added_amount active">3</div></button>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
   );
 }
 
