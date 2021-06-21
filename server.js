de3289e51eb6621b2d7e5d815be83d6776375a83
arseniypom <arpomazkov@bk.ru>
2021-06-21 12:36:27 +0300

    Deleted buggy server.use from server.js

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const middlewares = jsonServer.defaults({
 const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
-server.get('/*', (req,res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 server.use(router);
 
 server.listen(PORT, () => {
